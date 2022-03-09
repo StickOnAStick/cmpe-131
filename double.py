a7734b72349ef83cb875c2c2e666de40861d230d
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-03-08 19:53:03 -0800

    asdf

--- double.py
+++ double.py
@@ -1,8 +1,6 @@
 def doubler(func):
     def wrapper():
-        if(func() == None):
-            return
-        elif():
+       
             val = 2*func()
             return val
     return wrapper
