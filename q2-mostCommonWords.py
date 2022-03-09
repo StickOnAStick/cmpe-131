d907197462ea53a279df0de6b4cbb8b2bf9c9f77
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-03-08 16:54:58 -0800

    Moved HW2 to main

--- q2-mostCommonWords.py
+++ q2-mostCommonWords.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 #opens the file. the with statement here will automatically close it afterwards.
-with open("E:\School stuff\Code\cmpe 131\HW 2\q2\SampleText.txt") as input_file:
+with open("E:\School stuff\Code\cmpe 131\HW 2\Hw2\q2\SampleText.txt") as input_file:
     #builds a counter object for each line, then further by each word using line.split() which breaks words up by white spacing
     count = Counter(word for line in input_file
                          for word in line.split())
