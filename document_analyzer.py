e58286b9cadeff4f2bf78b72c1f02bba72bd21b9
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-03-10 22:59:37 -0800

    dscz

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,14 +1,17 @@
 from collections import Counter
+def parseFile():
+    with open("E:\School stuff\Code\cmpe 131\HW 2\Hw2\q2\SampleText.txt", "r") as file:
+        
+        words=[None] * 1
 
-#opens the file. the with statement here will automatically close it afterwards.
-with open("E:\School stuff\Code\cmpe 131\HW 2\Hw2\q2\SampleText.txt") as input_file:
-    #builds a counter object for each line, then further by each word using line.split() which breaks words up by white spacing
-    count = Counter(word for line in input_file
-                         for word in line.split())
+        for line in file:
+            for word in line.split():
+                words.append(word)
+    words.sort()
+    word_counts = Counter(words)
+    top_five = word_counts.most_common(5)
+    print(top_five)
 
-print(count.most_common(5))
 
-def run():
-    return count
 
-run()
+parseFile()
