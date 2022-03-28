af2dde914125b2c3b2e6d3ce7bc96ed2c837bf52
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-03-28 15:38:45 -0700

    asd

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,12 @@ from app import myobj
 #from app.login import LoginForm
 from flask import render_template, flash
 
+name = "Nicholas"
+city_names = ["Paris", "London", "Rome", "Thaiti"]
+
 @myobj.route('/')
 def home():
-    name = "Nicholas"
-    city_names = ["Paris", "London", "Rome", "Thaiti"]
+    
     return render_template("Home.html" , name = name , city_names = city_names)
     
     
