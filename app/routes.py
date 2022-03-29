d40cd058462feafd86fb13bcfc405cdff3ef6680
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-03-28 20:44:18 -0700

    Final?

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,21 @@
 from app import myobj
 #from app.login import LoginForm
-from flask import render_template, flash
+from flask import Flask, render_template, request, flash
+import re
 
 name = "Nicholas"
 city_names = ["Paris", "London", "Rome", "Thaiti"]
 
-@myobj.route('/')
+@myobj.route('/', methods = ['GET' , 'POST'])
 def home():
+        if request.method == 'POST':
+                city = request.form['input']
+                flash()
+                return render_template("Home.html" , name = name , city_names = city_names)
+        else:
+                return render_template("Home.html" , name = name , city_names = city_names)
     
-    return render_template("Home.html" , name = name , city_names = city_names)
-    
+
+
     
 
